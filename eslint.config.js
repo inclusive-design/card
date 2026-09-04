a71333f442ffdf91b78967d2f1705c256e1c3570
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-09-04 15:44:36 -0300

    feat: add documentation, remove @scope (#4)

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,18 @@ import eslintConfigInclusiveDesign from '@inclusive-design/eslint-config';
 export default defineConfig([
 	{
 		extends: [eslintConfigInclusiveDesign],
+		files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
 		rules: {
 			'new-cap': ['error', { capIsNewExceptionPattern: '^Elena' }],
 			'no-constant-binary-expression': 'off',
 			'unicorn/no-top-level-side-effects': 'off',
+			'jsdoc/check-tag-names': ['error', {
+				definedTags: [
+					'displayName',
+					'status',
+					'cssprop',
+				],
+			}],
 		},
 	},
 	{
